01ff22ad993d261eb83758aedc255c8058ddb907
Maya9awadh <125116838+Maya9awadh@users.noreply.github.com>
2023-05-11 10:58:35 +0400

    Update potd2.py

--- potd2.py
+++ potd2.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 """
 program to calculate the length and slop 
 of each vector in matrix.
@@ -17,35 +18,27 @@ matrix=[[-10,20,-10,-18],
 #transfer the matrix to np array
 a=np.array(matrix)
 
-#intialize list to store the length of each vector
-length=[]
-
-#initialize list to store the slop of each vector
-slop=[]
 
 #initialize list to store all y.
 y=[]
-
+print("        Length" "          ","Slop")
 #iterate over the row of matrix
 for i in range(5):
-    
+        #calculate the length of vector
+        l=m.sqrt((a[i][3]-a[i][1])**2 +(a[i][2]-a[i][0])**2)
+        print("S"+str(i),' ',l,end=' ')
         #calculate the slope of vector
         s=(a[i][3]-a[i][1])/(a[i][2]-a[i][0])
         #add the slop to the list
-        slop.append(s)
         
-        #calculate the length of vector
-        l=m.sqrt((a[i][3]-a[i][1])**2 +(a[i][2]-a[i][0])**2)
-        #add the length to the list
-        length.append(round(l,2))
+        print(' ',s)
+       
         
         #add all y to the list
         y.append(a[i][1])
         y.append(a[i][3])
-        
-        
-       
-print(length)   
-print(slop)
-print(max(y))
-print(min(y))
+          
+#print(length)   
+#print(slop)
+print("max y is: ",max(y))
+print("min of y:",min(y))
