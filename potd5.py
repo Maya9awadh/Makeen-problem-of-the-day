9459669ea3f0a8977862f13afda792585305014e
Maya9awadh <125116838+Maya9awadh@users.noreply.github.com>
2023-05-29 07:45:34 +0400

    Add files via upload

---
+++ potd5.py
@@ -0,0 +1,15 @@
+for i in range(1, 11):
+    print(" " * (10 - i), end="")
+    print("*" * i, end="")
+    print(" " *10, end="")
+    print("*" * i)
+
+ 
+print("*****     - - -           *****")
+print("*****     - - -           *****")
+
+for i in range(10, 0, -1):
+    print(" " * (10 - i), end="")
+    print("*" * i, end="")
+    print(" " * 10, end="")
+    print("*" * i)
